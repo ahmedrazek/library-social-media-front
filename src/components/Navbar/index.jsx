8f748b160f532a52d28e27534df6af7806b3cb84
ahmedrazek <ahmedrazek97@gmail.com>
2024-05-28 23:11:27 +0300

    add login page

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,5 +1,10 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 
 export default function Navbar() {
+  const location = useLocation();
+  console.log(location);
+  if (location.pathname == "/login" || location.pathname == "/signup")
+    return null;
   return <div>index</div>;
 }
