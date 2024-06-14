02d8d811d64f8d4a15e64cccb81bd9927904eb03
hala-emam <hala.emam141@gmail.com>
2024-06-14 14:46:05 +0300

    some changes in about us

--- src/components/Aboutus/index.jsx
+++ src/components/Aboutus/index.jsx
@@ -13,7 +13,7 @@ const About = () => {
           </div>
         </div>
         <div className="lg:w-1/2 lg:pl-12">
-          <h2 className="text-3xl font-bold mb-4 text-start text-primary">About Us</h2>
+          <h2 className="text-3xl font-bold mb-4 text-start  text-primary">About Us</h2>
           <p className="text-gray-700 mb-6">
             Welcome to our online community library, an integrated platform that provides you with access to a wide range of resources and knowledge from anywhere, at any time. Our goal is to facilitate access to knowledge and promote a love of reading and education for everyone in the community online. We offer you a diverse collection of e-books, audiobooks, and digital databases, in addition to educational programs and interactive online workshops for children and adults. Join us and explore a world of knowledge and educational opportunities available to you through your online community library.
           </p>
