3d1fd24bf7e8ae1753ac15ad95849551f1fc11b6
Jaklen22 <jaklenmagdy365@gmail.com>
2024-06-18 10:37:30 -0700

    some changes

--- src/store/userSlice.js
+++ src/store/userSlice.js
@@ -1,3 +1,4 @@
+
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
 import axios from "axios";
@@ -47,52 +48,4 @@ export const { setUser, logout } = userSlice.actions;
 export default userSlice.reducer;
 
 
-// // userSlice.js
-// import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-// import axios from "axios";
 
-// export const fetchUserProfile = createAsyncThunk(
-//   "user/fetchUserProfile",
-//   async () => {
-//     const res = await axios.get("/users/profile");
-//     return res.data;
-//   }
-// );
-
-// const initialState = {
-//   user: null,
-//   status: "idle",
-//   error: null,
-// };
-
-// const userSlice = createSlice({
-//   name: "user",
-//   initialState,
-//   reducers: {
-//     setUser: (state, action) => {
-//       state.user = action.payload;
-//     },
-//     logout: (state) => {
-//       state.user = null;
-//       state.status = "idle";
-//     },
-//   },
-//   extraReducers: (builder) => {
-//     builder
-//       .addCase(fetchUserProfile.pending, (state) => {
-//         state.status = "loading";
-//       })
-//       .addCase(fetchUserProfile.fulfilled, (state, action) => {
-//         state.status = "succeeded";
-//         state.user = action.payload;
-//       })
-//       .addCase(fetchUserProfile.rejected, (state, action) => {
-//         state.status = "failed";
-//         state.error = action.error.message;
-//       });
-//   },
-// });
-
-// export const { setUser, logout } = userSlice.actions;
-
-// export default userSlice.reducer;
