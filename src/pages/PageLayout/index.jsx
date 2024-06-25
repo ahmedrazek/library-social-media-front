850ba1b4af3d377b5dcee6529ece73ad4694f935
hala-emam <hala.emam141@gmail.com>
2024-06-25 09:38:07 +0300

    updates

--- src/pages/PageLayout/index.jsx
+++ src/pages/PageLayout/index.jsx
@@ -1,4 +1,4 @@
-<<<<<<< .merge_file_ig0Ivz
+
 import Navbar from "../../components/Navbar";
 import { Link, Navigate, NavLink, Outlet } from "react-router-dom";
 import Sidebar from "../../components/Sidebar";
@@ -19,27 +19,4 @@ const PageLayout = () => {
 };
 
 export default PageLayout;
-=======
-import Navbar from "../../components/Navbar";
-import { Link, Navigate, NavLink, Outlet } from "react-router-dom";
 
-import Sidebar from "../../components/Sidebar/index";
-const PageLayout = () => {
-  return (
-    <div>
-      <Navbar />
-      <div className="">
-        <Sidebar />
-
-
-
-        <div className="mt-20">
-          <Outlet className="space-y-20 " />
-        </div>
-      </div>
-    </div>
-  );
-};
-
-export default PageLayout;
->>>>>>> .merge_file_dDw52y
